827d86a734fe498eccb6f918d71163cbb58aed9e
Niklas Teich <n.teich@teich-datensysteme.de>
2019-12-15 17:55:43 +0100

    - initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MillProductMpn
+
+Shopware 6 plugin to display the mpn on the product detail page
+
+## System requirements
+
+* Shopware >= 6.0
+
+## Installation
+
+* install plugin
+* activate plugin
---
+++ src/MillProductMpn.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Mill\ProductMpn;
+
+use Shopware\Core\Framework\Plugin;
+
+/**
+ * Class MillProductMpn
+ *
+ * @package Mill\ProductMpn
+ */
+
+class MillProductMpn extends Plugin
+{
+
+
+}
